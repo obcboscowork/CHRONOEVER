6dbb48bf9658ade52b4b62e64826d3790e449ada
Oishik Bhanja Choudhury <oishikchoudhuryit@gmail.com>
2026-06-13 15:11:23 +0200

    Fix Productitem import capitalisation in LatestCollection and BestSeller

--- src/components/BestSeller.jsx
+++ src/components/BestSeller.jsx
@@ -25,7 +25,7 @@ useEffect(()=>{
         <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
            {
             bestSeller.map((item,index)=>(
-                <Productitem key={index} id={item._id} name={item.name} image={item.image} price={item.price}/>
+                <ProductItem key={index} id={item._id} name={item.name} image={item.image} price={item.price}/>
             ))
            } 
         </div>        
