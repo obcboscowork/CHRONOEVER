73be04a6a742011bbd1d2d14faddc5616edcb9d6
Oishik Bhanja Choudhury <oishikchoudhuryit@gmail.com>
2026-06-16 16:02:37 +0200

    Update README with live Vercel link and progress tracker

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { ShopContext } from '../context/ShopContext';
 
 const Product = () => {
+
+  const { productID } = useParams();
+  const { products } = useContext(ShopContext);
+  const [productData, setProductData] = useState(false);
+
+  const fetchProductData = async () => {
+    products.map((item) => {
+      if (item._id == productID) {
+        setProductData(item)
+        console.log(item);
+        return null;
+      }
+    })
+  }
+
+  useEffect(() => {
+    fetchProductData();
+  }, [productID])
+
   return (
     <div>
       
