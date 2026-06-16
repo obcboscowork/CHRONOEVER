c04728d7296f3d40e0285e386e0e1f2c68654113
Oishik Bhanja Choudhury <oishikchoudhuryit@gmail.com>
2026-06-16 21:52:37 +0200

    Update README with live Vercel link and progress tracker

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -7,11 +7,13 @@ const Product = () => {
   const { productID } = useParams();
   const { products } = useContext(ShopContext);
   const [productData, setProductData] = useState(false);
+  const [image, setImage] = useState('')
 
   const fetchProductData = async () => {
     products.map((item) => {
       if (item._id == productID) {
         setProductData(item)
+        setImage(item.image[0])
         console.log(item);
         return null;
       }
@@ -22,11 +24,34 @@ const Product = () => {
     fetchProductData();
   }, [productID])
 
-  return (
-    <div>
-      
+  return productData ? (
+    <div className='border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100'>
+      {/* Product Data */}
+      <div className='flex gap-12 sm:gap-12 flex-col sm:flex-row'>
+
+        {/*-------- Product Images------- */}
+        <div className='flex-1 flex flex-col-reverse gap-3 sm:flex-row'>
+          <div className='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full'>
+            {
+              productData.image.map((item, index) => (
+                <img onClick={() => setImage(item)} src={item} key={index} className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer' alt="" />
+              ))
+            }
+          </div>
+          {/* Main Image */}
+          <div className='w-full sm:w-[80%]'>
+            <img className='w-full h-auto' src={image} alt="" />
+          </div>
+        </div>
+
+        {/* -------Product Info ------- */}
+        <div className='flex-1'>
+            <h1 className='font-medium text-2xl mt-2'>{productData.name}</h1>
+        </div>
+      </div>
+
     </div>
-  )
+  ) : <div className='opacity-0'></div>
 }
 
 export default Product
